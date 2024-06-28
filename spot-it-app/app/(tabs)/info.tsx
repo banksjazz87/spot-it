f131bd76a7789e2c0f38c72242ab7fbfcc71b8ef
Chris <banksjazz87@gmail.com>
2024-06-28 06:41:54 -0400

    update: I have expo updated and is now running correctly on the ios simulator

--- spot-it-app/app/(tabs)/info.tsx
+++ spot-it-app/app/(tabs)/info.tsx
@@ -1,27 +1,128 @@
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView } from "react-native";
 import CharacterList from "../../components/CharacterList";
 
 
 
 export default function Info() {
 
-    
+    const pokemonImages = [
+			{
+				key: "abra",
+				url: require("../../assets/images/pokemon/abra.png"),
+				description: "Abra",
+			},
+			{
+				key: "blastoise",
+				url: require("../../assets/images/pokemon/blastoise.png"),
+				description: "Blastoise",
+			},
+			{
+				key: "bulbasaur",
+				url: require("../../assets/images/pokemon/bulbasaur.png"),
+				description: "Bulbasaur",
+			},
+			{
+				key: "caterpie",
+				url: require("../../assets/images/pokemon/caterpie.png"),
+				description: "Caterpie",
+			},
+			{
+				key: "charizard",
+				url: require("../../assets/images/pokemon/charizard.png"),
+				description: "Charizard",
+			},
+			{
+				key: "charmander",
+				url: require("../../assets/images/pokemon/charmander.png"),
+				description: "Charmander",
+			},
+			{
+				key: "charmeleon",
+				url: require("../../assets/images/pokemon/charmeleon.png"),
+				description: "Charmeleon",
+			},
+			{
+				key: "diglett",
+				url: require("../../assets/images/pokemon/diglett.png"),
+				description: "Diglett",
+			},
+			{
+				key: "geodude",
+				url: require("../../assets/images/pokemon/geodude.png"),
+				description: "Geodude",
+			},
+			{
+				key: "horsea",
+				url: require("../../assets/images/pokemon/horsea.png"),
+				description: "Horsea",
+			},
+			{
+				key: "jigglypuff",
+				url: require("../../assets/images/pokemon/jigglypuff.png"),
+				description: "Jigglypuff",
+			},
+			{
+				key: "koffing",
+				url: require("../../assets/images/pokemon/koffing.png"),
+				description: "Koffing",
+			},
+			{
+				key: "marowak",
+				url: require("../../assets/images/pokemon/marowak.png"),
+				description: "Marowak",
+			},
+			{
+				key: "onix",
+				url: require("../../assets/images/pokemon/onix.png"),
+				description: "Onix",
+			},
+			{
+				key: "pikachu",
+				url: require("../../assets/images/pokemon/pikachu.png"),
+				description: "Pikachu",
+			},
+			{
+				key: "psyduck",
+				url: require("../../assets/images/pokemon/psyduck.png"),
+				description: "Psyduck",
+			},
+			{
+				key: "rattata",
+				url: require("../../assets/images/pokemon/rattata.png"),
+				description: "Rattata",
+			},
+			{
+				key: "sandslash",
+				url: require("../../assets/images/pokemon/sandslash.png"),
+				description: "Sandslash",
+			},
+			{
+				key: "squirtle",
+				url: require("../../assets/images/pokemon/squirtle.png"),
+				description: "Squirtle",
+			},
+			{
+				key: "vulpix",
+				url: require("../../assets/images/pokemon/vulpix.png"),
+				description: "Vulpix",
+			},
+			{
+				key: "wartortle",
+				url: require("../../assets/images/pokemon/wartortle.png"),
+				description: "Wartortle",
+			},
+			{
+				key: "weezing",
+				url: require("../../assets/images/pokemon/weezing.png"),
+				description: "Weezing",
+			},
+		];
 
     
     return (
 			<View>
 				<Text>This will have information about each image used.</Text>
-				<CharacterList
-                properties={
-                    [
-                        {
-                            key: "test",
-                            url: require("../../assets/images/pokemon/abra.png"),
-                            description: "Abra",
-                        }
-                    ]
-                }
-				/>
+				<CharacterList properties={pokemonImages} />
 			</View>
 		);
 }
--- spot-it-app/components/CharacterList.tsx
+++ spot-it-app/components/CharacterList.tsx
@@ -1,5 +1,4 @@
-import { Image } from "expo-image";
-import { FlatList, StyleSheet, Text, View, ScrollView } from "react-native";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 type ImageProperties = {
@@ -13,23 +12,22 @@ type CharacterListProps = {
 };
 
 export default function CharacterList({ properties }: CharacterListProps) {
-	return (
-		<View>
+    return (
 			<FlatList
 				data={properties}
 				renderItem={({ item }) => (
-					<>
+					<View>
 						<Image
 							source={item.url}
 							style={styles.image}
 							key={item.key}
 						/>
 						<Text>{item.description}</Text>
-					</>
+					</View>
 				)}
+				keyExtractor={(item) => item.key}
 			/>
-		</View>
-	);
+		);
 }
 
 const styles = StyleSheet.create({
@@ -37,5 +35,5 @@ const styles = StyleSheet.create({
 		width: 100,
 		height: 100,
 		marginRight: 20,
-	},
+    }
 });
