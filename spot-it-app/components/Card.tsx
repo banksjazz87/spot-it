d4c0f7b349fb936198808cbcf0c95f5881d5a999
Chris <banksjazz87@gmail.com>
2024-07-03 14:03:26 -0400

    feature: creating a layout for the cards

--- spot-it-app/components/Card.tsx
+++ spot-it-app/components/Card.tsx
@@ -1,11 +1,20 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { StyleClasses } from '../constants/StyleClasses';
+import CharacterCardGrid from './CharacterCardGrid';
+import CharacterImages from '../constants/CharacterImages';
 
 
 export function Card() {
+	const row1 = CharacterImages.slice(0, 2);
+	const row2 = CharacterImages.slice(3, 7);
+	const row3 = CharacterImages.slice(6, 7);
+
+	const grid = [row1, row2, row3];
     return (
 			<View style={[styles.card, StyleClasses.boxShadow]}>
-				<Text>Testing 123</Text>
+				<CharacterCardGrid
+					items={grid}
+				/>
 			</View>
 		);
 }
---
+++ spot-it-app/components/CharacterCardGrid.tsx
@@ -0,0 +1,48 @@
+import { Image, View, StyleSheet } from 'react-native';
+
+type ImageProperties = {
+    key: string;
+    url: any;
+    description: string;
+}
+
+type CharacterCardProps = {
+    items: ImageProperties[][];
+} 
+
+export default function CharacterCardGrid({items}: CharacterCardProps): JSX.Element[] {
+    
+    return items.map((x: ImageProperties[], y: number): JSX.Element => {
+        return (
+            <View
+                id={`row_${y}`}
+                key={`row_${y}`}
+                style={styles.cardRow}
+            >
+                {
+                    x.map((a: ImageProperties, b: number): JSX.Element =>
+                        <Image
+                            source={a.url}
+                            style={styles.image}
+                            key={`image_${y}_${b}`}
+                            resizeMode={'contain'}
+                        >
+                        </Image>
+                    )
+                }
+            </View>
+        )
+    });
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 50,
+        height: 50
+    },
+    cardRow: {
+        display: 'flex', 
+        flexDirection: 'row',
+        flexWrap: 'nowrap'
+    }
+});
