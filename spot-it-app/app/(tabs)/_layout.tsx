88c53a64ae2b5da492a0ab172676e8100430832b
Chris <banksjazz87@gmail.com>
2024-08-28 12:35:08 -0400

    feature: working on conditionally rendering the nav menu, based on logged in

--- spot-it-app/app/(tabs)/_layout.tsx
+++ spot-it-app/app/(tabs)/_layout.tsx
@@ -1,57 +1,105 @@
 import { Tabs } from "expo-router";
-import React from "react";
+import {useEffect, useState} from "react";
 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/lib/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import "expo-dev-client";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+interface User {
+	username: string;
+	email: string;
+	loggedIn: boolean;
+}
 
 export default function TabLayout() {
 	const colorScheme = useColorScheme();
+	const [currentUser, setCurrentUser] = useState<User>({
+		username: '',
+		email: '',
+		loggedIn: false
+	});
+	
+	
+	const getUserInfo = async (): Promise<User | null> => {
+		try {
+			const jsonValue = await AsyncStorage.getItem('userInfo');
 
-	return (
-		<Tabs
-			screenOptions={{
-				tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-				headerShown: false,
-			}}
-		>
-			<Tabs.Screen
-				name="index"
-				options={{
-					title: "Home",
-					tabBarIcon: ({ color, focused }) => (
-						<TabBarIcon
-							name={focused ? "home" : "home-outline"}
-							color={color}
-						/>
-					),
+			if (jsonValue) {
+				const parsed: User = JSON.parse(jsonValue);
+				
+				return parsed;
+
+			} else {
+				return null;
+			}
+		} catch (e) {
+
+			console.log('Error in getting async storage ', e);
+			return null;
+		}
+	}
+
+	useEffect((): void => {
+		getUserInfo()
+			.then((data) => {
+				if (data !== null) {
+					setCurrentUser({
+						...currentUser,
+						username: data.username,
+						email: data.email,
+						loggedIn: true,
+					});
+				}
+			});
+	}, []);
+
+		return (
+			<Tabs
+				screenOptions={{
+					tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+					headerShown: false,
 				}}
-			/>
-			<Tabs.Screen
-				name="game"
-				options={{
-					title: "Game",
-					tabBarIcon: ({ color, focused }) => (
-						<TabBarIcon
-							name={focused ? "game-controller" : "game-controller-outline"}
-							color={color}
-						/>
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="info"
-				options={{
-					title: "Info",
-					tabBarIcon: ({ color, focused }) => (
-						<TabBarIcon
-							name={focused ? "information-circle" : "information-circle-outline"}
-							color={color}
-						/>
-					),
-				}}
-			/>
-		</Tabs>
-	);
+			>
+				<Tabs.Screen
+					name="index"
+					options={{
+						title: "Home",
+						tabBarIcon: ({ color, focused }) => (
+							<TabBarIcon
+								name={focused ? "home" : "home-outline"}
+								color={color}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="game"
+					options={{
+						title: "Game",
+						tabBarIcon: ({ color, focused }) => (
+							<TabBarIcon
+								name={focused ? "game-controller" : "game-controller-outline"}
+								color={color}
+							/>
+						),
+						headerShown: false 
+					}}
+				/>
+				<Tabs.Screen
+					name="info"
+					options={{
+						title: "Info",
+						tabBarIcon: ({ color, focused }) => (
+							<TabBarIcon
+								name={focused ? "information-circle" : "information-circle-outline"}
+								color={color}
+							/>
+						),
+					}}
+				/>
+			</Tabs>
+		);
 }
