a07b31c2b2777f639bd6de2e735eaacab33ff01f
Chris <banksjazz87@gmail.com>
2024-08-30 08:30:56 -0400

    feature: wrote the change handler needed for the text inputs for the login

--- spot-it-app/components/home/Login.tsx
+++ spot-it-app/components/home/Login.tsx
@@ -1,47 +1,64 @@
-import {useState} from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
-import  PrimaryButton from "../global/PrimaryButton"
-
+import { useState } from "react";
+import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
+import PrimaryButton from "../global/PrimaryButton";
 
 interface UserLogin {
-    email: string;
-    password: string;
+	email: string;
+	password: string;
 }
 
 export default function Login() {
+	const [loginUser, setLoginUser] = useState<UserLogin>({
+		email: "Email",
+		password: "Password",
+	});
 
-    const [loginUser, setLoginUser] = useState<UserLogin>({
-        email: "Email",
-        password: "Password"
-    });
+	const loginChangeHandler = (text: string, key: string): void => {
+		setLoginUser({
+			...loginUser,
+			[key as keyof UserLogin]: text,
+		});
+	};
 
-    
-    return (
-        <View style={styles.loginContainer}>
-            <TextInput
-                editable
-                numberOfLines={1}
-                maxLength={40}
-                autoComplete={"email"}
-                value={"Email"} 
-                style={styles.input}
-            />
-            <TextInput
-                editable
-                numberOfLines={1}
-                maxLength={40}
-                autoComplete={"password"}
-                value={"Password"}
-                style={styles.input}
-            />
-            <PrimaryButton
-                text="Log In"
-                method={() => console.log('Clicked')}
-                style={{paddingTop: 10, paddingBottom: 10}}
-            />
+	return (
+		<View style={styles.loginContainer}>
+			<TextInput
+				editable
+				numberOfLines={1}
+				maxLength={40}
+				autoComplete={"email"}
+				value={loginUser.email}
+				style={styles.input}
+				onChangeText={(text: string) => loginChangeHandler(text, "email")}
+				autoCapitalize="none"
+			/>
+			<TextInput
+				editable
+				numberOfLines={1}
+				maxLength={40}
+				autoComplete={"password"}
+				value={loginUser.password}
+				style={styles.input}
+				onChangeText={(text: string) => loginChangeHandler(text, "password")}
+				autoCapitalize="none"
+			/>
 
-        </View>
-    )
+			<View style={{ display: "flex", flexDirection: "row", columnGap: 10 }}>
+				<Pressable>
+					<Text>Forgot your password?</Text>
+				</Pressable>
+				<Pressable>
+					<Text>New Here</Text>
+				</Pressable>
+			</View>
+
+			<PrimaryButton
+				text="Log In"
+				method={() => console.log("Clicked")}
+				style={{ paddingTop: 10, paddingBottom: 10 }}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
