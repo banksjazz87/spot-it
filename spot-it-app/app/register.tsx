7cb96f90f4146014e518725f0ab26007f3f420f0
Chris <banksjazz87@gmail.com>
2024-09-12 08:48:46 -0400

    feature: I have a feature in place to hide and show the password

--- spot-it-app/app/register.tsx
+++ spot-it-app/app/register.tsx
@@ -17,6 +17,7 @@ export default function Register() {
 		password: "Password",
 		verifyPassword: "Verify Password",
 	});
+	const [hidePassword, setHidePassword] = useState<boolean>(true);
 
 	return (
 		<View style={styles.wrapper}>
@@ -24,6 +25,8 @@ export default function Register() {
 				<Text>Create an account</Text>
 				<TextInput
 					editable
+					textContentType="emailAddress"
+					inputMode="email"
 					numberOfLines={1}
 					maxLength={40}
 					value={newUser.email}
@@ -31,9 +34,50 @@ export default function Register() {
 					autoCapitalize="none"
 					style={[StyleClasses.textInput]}
 				/>
-				<Pressable onPress={(e: GestureResponderEvent): void => router.navigate("/")}>
-					<Text>Return to Login</Text>
-				</Pressable>
+				<TextInput
+					editable
+					textContentType="username"
+					inputMode="text"
+					numberOfLines={1}
+					maxLength={40}
+					value={newUser.userName}
+					onChangeText={(text) => setNewUser({ ...newUser, userName: text })}
+					autoCapitalize="none"
+					style={[StyleClasses.textInput]}
+				/>
+				<TextInput
+					editable
+					textContentType="password"
+					inputMode="text"
+					secureTextEntry={hidePassword}
+					numberOfLines={1}
+					maxLength={40}
+					value={newUser.password}
+					onChangeText={(text) => setNewUser({ ...newUser, password: text })}
+					autoCapitalize="none"
+					style={[StyleClasses.textInput]}
+				/>
+				<TextInput
+					editable
+					numberOfLines={1}
+					textContentType="password"
+					inputMode="text"
+					secureTextEntry={hidePassword}
+					maxLength={40}
+					value={newUser.verifyPassword}
+					onChangeText={(text) => setNewUser({ ...newUser, verifyPassword: text })}
+					autoCapitalize="none"
+					style={[StyleClasses.textInput]}
+				/>
+				<View>
+					<Pressable onPress={(e: GestureResponderEvent): void => router.navigate("/")}>
+						<Text>Return to Login</Text>
+					</Pressable>
+
+					<Pressable onPress={(e: GestureResponderEvent): void => setHidePassword(!hidePassword)}>
+						<Text>{hidePassword ? "Show Password" : "Hide Password"}</Text>
+					</Pressable>
+				</View>
 			</View>
 		</View>
 	);
