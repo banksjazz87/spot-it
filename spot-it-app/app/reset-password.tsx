922477b0ca6919dddba24494ca758b7ebcd9d1a5
Chris <banksjazz87@gmail.com>
2025-01-14 14:14:04 -0500

    feature: continuing to build out the custom alert component

--- spot-it-app/app/reset-password.tsx
+++ spot-it-app/app/reset-password.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { View, Text, TextInput, StyleSheet, Pressable, Alert, GestureResponderEvent, Modal } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PrimaryButton from "@/components/global/PrimaryButton";
@@ -10,12 +10,15 @@ import AppModal from "@/components/global/AppModal";
 
 export default function ResetPassword(): JSX.Element {
 	const [userEmail, setUserEmail] = useState<string>("Email");
-    const [modalVisible, setModalVisible] = useState<boolean>(false);
-    const [modalText, setModalText] = useState<string>('');
+	const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+	const [modalText, setModalText] = useState<string>("");
+	const [validSubmission, setValidSubmission] = useState<boolean>(false);
 
 	const emailChangeHandler = (text: string): void => {
 		setUserEmail(text);
 	};
+
+	useEffect((): void => console.log(isModalVisible), [isModalVisible]);
 
 	//Used to get the user details based on the provided email, this is fired in the submit handler.
 	const getUserDetails = async (email: string): Promise<APIResponse<EmailData> | undefined> => {
@@ -58,6 +61,10 @@ export default function ResetPassword(): JSX.Element {
 							if (typeof final !== "undefined" && final.status === 200) {
 								console.log(`The reset email has been sent to the provided email address ${neededData.email}.`);
 
+								setIsModalVisible(true);
+								setModalText("Your password has been reset! Check your email for the new password and use the link provided to log in. Didn’t see the email? Check your spam folder. Need help? Contact support.");
+								setValidSubmission(true);
+
 								//Failed in reaching out to the API
 							} else if (typeof final === "undefined") {
 								return final;
@@ -89,45 +96,82 @@ export default function ResetPassword(): JSX.Element {
 
 	return (
 		<SafeAreaView style={[StyleClasses.loginContainer, { justifyContent: "center", flex: 1 }]}>
-				<Text style={[StyleClasses.headingOne, { textAlign: "left", textTransform: "uppercase", fontWeight: 700 }]}>Request New Password</Text>
-				<TextInput
-					editable
-					numberOfLines={1}
-					maxLength={40}
-					autoComplete={"email"}
-					value={userEmail}
-					style={StyleClasses.textInput}
-					onChangeText={(text: string): void => emailChangeHandler(text)}
-					onPressIn={(): void => setUserEmail("")}
-					autoCapitalize="none"
-				/>
-
-				<View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", columnGap: 10 }}>
-					<Pressable
-						onPress={(e: GestureResponderEvent): void => {
-							router.navigate("/");
-						}}
-					>
-						<Text>Back to Login</Text>
-					</Pressable>
-
-					<Pressable
-						onPress={(e: GestureResponderEvent): void => {
-							router.navigate("/register");
-						}}
-					>
-						<Text>Create New Account</Text>
-					</Pressable>
-				</View>
-
-				<PrimaryButton
-					text="Submit"
-					method={() => {
-						submitHandler();
+			{isModalVisible && (
+				<AppModal
+					modalVisible={isModalVisible}
+					visibleHandler={(): void => setIsModalVisible(!isModalVisible)}
+					message={modalText}
+					acceptText={"Okay"}
+					acceptHandler={(): void => {
+						if (validSubmission) {
+							setIsModalVisible(false);
+						} else {
+							setIsModalVisible(false);
+						}
 					}}
-					style={{ paddingTop: 10, paddingBottom: 10 }}
-            />
-            
+					closeHandler={(): void => setIsModalVisible(false)}
+					rejectText="Cancel"
+					rejectHandler={(): void => setIsModalVisible(false)}
+				/>
+			)}
+
+
+			<Text style={[StyleClasses.headingOne, { textAlign: "left", textTransform: "uppercase", fontWeight: 700 }]}>Request New Password</Text>
+			<TextInput
+				editable
+				numberOfLines={1}
+				maxLength={40}
+				autoComplete={"email"}
+				value={userEmail}
+				style={StyleClasses.textInput}
+				onChangeText={(text: string): void => emailChangeHandler(text)}
+				onPressIn={(): void => setUserEmail("")}
+				autoCapitalize="none"
+			/>
+
+			<View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", columnGap: 10 }}>
+				<Pressable
+					onPress={(e: GestureResponderEvent): void => {
+						router.navigate("/");
+					}}
+				>
+					<Text>Back to Login</Text>
+				</Pressable>
+
+				<Pressable
+					onPress={(e: GestureResponderEvent): void => {
+						router.navigate("/register");
+					}}
+				>
+					<Text>Create New Account</Text>
+				</Pressable>
+			</View>
+
+			<PrimaryButton
+				text="Submit"
+				method={() => {
+					submitHandler();
+				}}
+				style={{ paddingTop: 10, paddingBottom: 10 }}
+			/>
 		</SafeAreaView>
 	);
 }
+
+const styles = StyleSheet.create({
+	innerContainer: {
+		display: "flex",
+		flexDirection: "column",
+		rowGap: 15,
+		padding: 20,
+		width: 400,
+	},
+	modalContainer: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "rgba(0, 0, 0, .8)",
+	},
+});
+
+
--- spot-it-app/components/global/AppModal.tsx
+++ spot-it-app/components/global/AppModal.tsx
@@ -1,46 +1,49 @@
-import { View, Modal, StyleSheet, Text, Pressable } from 'react-native';
-
+import { View, Modal, StyleSheet, Text, Pressable } from "react-native";
 
 interface AppModalProps {
 	modalVisible: boolean;
 	visibleHandler: Function;
 	message: string;
 	acceptText: string;
-    acceptHandler: Function;
-    closeHandler: Function;
+	acceptHandler: Function;
+	closeHandler: Function;
 	rejectText?: string;
 	rejectHandler?: Function;
 }
 
-export default function AppModal({modalVisible, visibleHandler, message, acceptText, acceptHandler, rejectText, rejectHandler, closeHandler}: AppModalProps): React.JSX.Element {
-    return (
-        <View style={styles.modalContainer}>
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={(): void => {
-                    visibleHandler();
-                }}
-            >
-                <View style={styles.modalContainer}>
-                    <Text>{message}</Text>
-                    <Pressable
-                        onPress={(): void => acceptHandler()}
-                    >
-                        <Text>{acceptText}</Text>
-                    </Pressable>
-                </View>
-            </Modal>
-        </View>
-    );
-} 
+export default function AppModal({ modalVisible, visibleHandler, message, acceptText, acceptHandler, rejectText, rejectHandler, closeHandler }: AppModalProps): React.JSX.Element {
+	return (
+		<Modal
+			animationType="slide"
+			transparent={true}
+			visible={modalVisible}
+			onRequestClose={(): void => {
+				visibleHandler();
+			}}
+		>
+			<View style={styles.modalContainer}>
+				<View style={styles.modalBox}>
+					<Text>{message}</Text>
+					<Pressable onPress={(): void => acceptHandler()}>
+						<Text>{acceptText}</Text>
+					</Pressable>
+				</View>
+			</View>
+		</Modal>
+	);
+}
 
 const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'rgba(0, 0, 0, .8)'
-    }
-})
+	modalContainer: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "rgba(0, 0, 0, .2)",
+		color: "white",
+	},
+
+	modalBox: {
+		backgroundColor: "white",
+		color: "black",
+	},
+});
