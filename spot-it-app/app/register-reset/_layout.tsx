82445a362ef36fcfe711a535f2127dafefbd852a
Chris <banksjazz87@gmail.com>
2025-01-29 13:56:26 -0500

    style: working on the styling for the stack headers

--- spot-it-app/app/register-reset/_layout.tsx
+++ spot-it-app/app/register-reset/_layout.tsx
@@ -1,55 +1,63 @@
-import { Stack, router } from 'expo-router';
-import { Pressable, Text } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { Stack, router } from "expo-router";
+import { Pressable, Text, View, StyleSheet } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
+interface CustomTitleProps {
+	styleProps: Object;
+	heading: string;
+}
 
-export default function Layout() {
-    return (
-			<Stack
-				screenOptions={{
-					headerStyle: {
-						backgroundColor: "black",
-					},
-					headerBackButtonMenuEnabled: true,
-					headerShadowVisible: true,
-					headerTitleAlign: "center",
-					headerLargeTitle: true,
-					headerTintColor: "white",
-					headerTitleStyle: {
-						fontWeight: "bold",
-					},
-					headerRight: () => (
-                        <Pressable
-                            onPress={() => router.navigate("/")}
-                            style={{
-                                display: 'flex',
-                                flex: 1,
-                                alignItems: 'flex-end',
-                                paddingRight: 40,
-                                bottom: 0
-                            }}
-                        >
-                            <Ionicons
-                                name="log-in-outline"
-                                size={36}
-                                color="white"
-                            />
-						</Pressable>
+export default function Layout(): React.JSX.Element {
+
+	return (
+		<Stack>
+			<Stack.Screen
+				name="reset-password"
+				options={{
+					header: () => (
+						<View style={styles.headerContainer}>
+							<Text style={styles.whiteHeading}>Reset Password</Text>
+							<Pressable
+								onPress={() => router.navigate("/")}
+							>
+								<Ionicons
+									name="log-in-outline"
+									size={36}
+									color="white"
+								/>
+							</Pressable>
+						</View>
 					),
 				}}
-			>
-				<Stack.Screen
-					name="reset-password"
-					options={{
-						title: "Reset Password",
-					}}
-				/>
-				<Stack.Screen
-					name="register"
-					options={{
-						title: "New Account",
-					}}
-				/>
-			</Stack>
-		);
+			/>
+			<Stack.Screen
+				name="register"
+				options={{
+					title: "New Account",
+				}}
+			/>
+		</Stack>
+	);
 }
+
+
+const styles = StyleSheet.create({
+	whiteHeading: {
+		color: "white",
+		fontSize: 32,
+		fontWeight: 600,
+		textTransform: "uppercase",
+    },
+    
+	headerContainer: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		paddingBottom: 10,
+		paddingTop: 30,
+        backgroundColor: "black",
+        paddingLeft: 10,
+        paddingRight: 10, 
+        alignItems: 'center'
+	},
+});
